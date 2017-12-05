12f6d774cb0e564a8de43a899f26616fd856a5e6
Zlo <patriot1394@gmale.com>
2017-12-05 19:05:28 +0300

    get ad

--- index.js
+++ index.js
@@ -1,13 +1,87 @@
 var TelegramBot = require('node-telegram-bot-api');
-console.log('STOP PLEASE');
+var MongoClient = require('mongodb').MongoClient;
+
 //установим токен
-/*
 var token = '449042193:AAG_rxZQdbcTot2NmL1rZJSUtcdHReoIgYs';
 
 //опрос сервера
 var bot = new TelegramBot(token,{polling:true});
 
 var notes = [];
+var url = "mongodb://Patriot:strelok@cluster0-shard-00-00-opb6q.mongodb.net:27017,cluster0-shard-00-01-opb6q.mongodb.net:27017,cluster0-shard-00-02-opb6q.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+//var uri = "mongodb://Patriot:strelok@cluster0-shard-00-00-opb6q.mongodb.net:27017 ,cluster0-shard-00-01-opb6q.mongodb.net:27017 , cluster0-shard-00-02-opb6q.mongodb.net:27017 /admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+MongoClient.connect(url, function(err, db) {
+    if(err){
+        return console.log(err);
+    }
+    db.close();
+});
+
+bot.onText(/\/ad (.+) i (.+)/, (msg, match) => {
+    
+    MongoClient.connect(url, function(err, db) {
+        if(err){
+            return console.log(err);
+        }
+        else{
+            const chatId = msg.chat.id;
+            var top = match[1]; 
+            var message = match[2];
+            var post = {
+                top: top,
+                message: message
+                };
+            var messages = db.collection("messages");
+            var err = messages.insertOne(post,function(err, result){
+                if(err){
+                    return console.log(err);
+                }
+                console.log(result.ops);
+                var str = 'Add '+post.top + " "+ post.message;
+                bot.sendMessage(chatId, str);
+                db.close();
+            });
+        }
+    });
+});
+
+bot.onText(/\/get_all/, (msg, match) => {
+    MongoClient.connect(url, function(err, db){
+        if(err){
+            return console.log(err);
+        }
+        db.collection("messages").find().toArray(function(err, results){
+            const chatId = msg.chat.id;
+            for(var i=0;i<results.length;i++){
+                var str = i + " " + results[i].top + " " + results[i].message;
+                bot.sendMessage(chatId, str);
+            }
+
+            console.log(results);
+            db.close();
+        });
+    });
+});
+
+bot.onText(/\/get (.+)/, (msg, match) => {
+    MongoClient.connect(url, function(err, db){
+        if(err){
+            return console.log(err);
+        }
+        const chatId = msg.chat.id;
+        const top = match[1]; 
+        db.collection("messages").find({top: top}).toArray(function(err, results){
+            const chatId = msg.chat.id;
+            for(var i=0;i<results.length;i++){
+                var str = i + " " + results[i].top + " " + results[i].message;
+                bot.sendMessage(chatId, str);
+            }
+
+            console.log(results);
+            db.close();
+        });
+    });
+});
 
 setInterval(function(){
     for (var i = 0; i < notes.length; i++){
@@ -44,7 +118,7 @@ bot.onText(/\/help (.+)/, (msg, match) => {
     bot.sendMessage(chatId, resp);
 });
 
-bot.onText(/\/напомни (.+) в (.+)/, function (msg, match) {
+bot.onText(/\/note (.+) in (.+)/, function (msg, match) {
     var userId = msg.from.id;
     var text = match[1];
     var time = match[2];
@@ -52,4 +126,4 @@ bot.onText(/\/напомни (.+) в (.+)/, function (msg, match) {
     notes.push( { 'uid':userId, 'time':time, 'text':text } );
 
     bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
-});*/
+});
